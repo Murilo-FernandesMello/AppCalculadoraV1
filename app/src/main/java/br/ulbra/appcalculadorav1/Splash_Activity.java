aa5a49d6080f5bb4e9e2f52bdd1ecd902fcf43d5
Murilo-FernandesMello <murilofmello07@gmail.com>
2022-10-20 19:35:18 -0300

    First Commit

---
+++ app/src/main/java/br/ulbra/appcalculadorav1/Splash_Activity.java
@@ -0,0 +1,31 @@
+package br.ulbra.appcalculadorav1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mostrarMainActivity();
+            }
+        }, 2000);
+    }
+
+    private void mostrarMainActivity() {
+        Intent intent = new Intent(
+                Splash_Activity.this,MainActivity.class
+        );
+        startActivity(intent);
+        finish();
+    }
+}
